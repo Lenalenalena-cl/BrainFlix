cca10812d8c9a0e44dedf5a4a93d6882feafb03f
Carrie Li <carriexuejiao.li@gmail.com>
2024-09-22 18:21:10 -0400

    set up header, video and movie detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.scss'
+import Header from './components/Header/Header'
+import VideoPlayerGeneral from './components/VideoPlayerGeneral/VideoPlayerGeneral'
+import videos from './data/video-details.json'
+import VideoPlayerDetails from './components/VideoPlayerDetails/VideoPlayerDetails'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [video, setVideo] = useState(videos[0])
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header/>
+      <VideoPlayerGeneral
+      image={video.image}
+      duration={video.duration}
+      video={video.video}
+      timestamp={video.timestamp}/>
+      <VideoPlayerDetails
+      title={video.title} 
+      channel={video.channel}
+      description={video.description}
+      views={video.views}
+      likes={video.likes}
+      timestamp={video.timestamp}
+      />
     </>
   )
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,22 @@
+import "./Header.scss"
+import logo from "../../assets/images/BrainFlix-logo.svg"
+
+function Header(){
+    return(
+<head className="header">
+    <img className="header__logo" src={logo} alt="BrainFlix Logo" />
+    <div className="header__content">
+        <div className="header__search-profile">
+            <input className="header__search-bar" type="text" placeholder="Search" />
+            <img className="header__profile-pic header__profile-pic--mobile" src="src/assets/images/Mohan-muruge.jpg" alt="side profile of Mohan" />
+        </div>
+        <button className="header__button--upload" type="submit">Upload</button>
+        <img className="header__profile-pic header__profile-pic--desktop" src="src/assets/images/Mohan-muruge.jpg" alt="side profile of Mohan" />
+    </div>
+    
+</head>
+    )
+    
+}
+
+export default Header;
